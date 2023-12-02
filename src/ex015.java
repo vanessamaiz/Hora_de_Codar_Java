9eea9eda82f14ebc78bd1cbba0c0dbe3f0a632d7
Vanessa Maiz <142240200+vanessamaiz@users.noreply.github.com>
2023-12-02 19:19:27 -0300

    Update ex015.java

--- src/ex015.java
+++ src/ex015.java
@@ -1,22 +1,20 @@
 public class ex015 {
     public static void main(String[] args){
         
-        double alturaAnacleto = 1.50;  // Altura inicial de Anacleto em metros
-        double crescimentoAnacleto = 0.02;  // Crescimento anual de Anacleto em metros
+        double alturaAnacleto = 1.50; 
+        double crescimentoAnacleto = 0.02; 
 
-        double alturaFelisberto = 1.10;  // Altura inicial de Felisberto em metros
-        double crescimentoFelisberto = 0.03;  // Crescimento anual de Felisberto em metros
+        double alturaFelisberto = 1.10; 
+        double crescimentoFelisberto = 0.03; 
 
         int anos = 0;
 
-        // Loop para calcular quantos anos serão necessários
         while (alturaFelisberto <= alturaAnacleto) {
             alturaAnacleto += crescimentoAnacleto;
             alturaFelisberto += crescimentoFelisberto;
             anos++;
         }
 
-        // Exibir o resultado
         System.out.println("Serão necessários " + anos + " anos para Felisberto ser maior que Anacleto.");
     }
 }
